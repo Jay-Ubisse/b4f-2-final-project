24bbe841c50db4e3df7ef4735083f2ceef65219c
Jay-Ubisse <joaquimubisse@gmail.com>
2025-06-09 14:26:02 +0200

    feat: create project structure

---
+++ backend/src/server.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+
+const app = express();
+dotenv.config();
+app.use(express.json());
+app.use(cors());
+
+const host = process.env.HOST || "http://localhost";
+const port = process.env.PORT || 3000;
+
+mongoose
+  .connect(process.env.BD_URI as string)
+  .then(() => console.log("BD conectado com sucesso!"))
+  .catch((error) =>
+    console.log("Ocorreu um erro ao contectar com a DB: ", error)
+  );
+
+app.listen(port, () => console.log(`Server running on ${host}:${port}`));
