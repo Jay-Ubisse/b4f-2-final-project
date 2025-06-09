0f9b5f0fafe21e42dfc904b958d595f1ac41f31a
Jorge Langa <jorgebernardolanga@gmail.com>
2025-06-09 16:16:14 +0200

    feat:Create interface  userProps

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -2,4 +2,5 @@ import { Request, Response } from 'express';
 
 export const login =(req: Request, res: Response) => {
 
+    
 }
---
+++ backend/src/types/types.ts
@@ -0,0 +1,7 @@
+
+export interface userProps {
+name:string,
+email:string,
+password:string,
+role: 'Admin' | 'Costumer'
+}
