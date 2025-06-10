4eb9d5e1d16dd0a14790af82abfbe404795ed9da
Afonso Relane <afonso.relane@gmail.com>
2025-06-10 15:17:50 +0200

    feat: order Schema

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,5 +1,10 @@
 import mongoose, { Document, Schema } from "mongoose";
-import { CategoryProps, ProductProps, UserProps } from "../types/user.js";
+import {
+  CategoryProps,
+  ProductProps,
+  UserProps,
+  OrderProps,
+} from "../types/user.js";
 
 /* ================== USER =============== */
 
@@ -48,3 +53,31 @@ const productSchema = new Schema<ProductProps>({
 });
 
 export const Product = mongoose.model<ProductProps>("Product", productSchema);
+
+/* ===================== ORDER =================== */
+
+const orderSchema = new Schema<OrderProps>({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  userId: { type: String, required: true },
+  items: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+  ],
+  status: {
+    type: String,
+    required: true,
+    enum: ["pendente", "enviado", "entregue", "cancelado"],
+    default: "pending",
+  },
+  total: { type: Number, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+export const Order = mongoose.model<OrderProps>("Order", orderSchema);
