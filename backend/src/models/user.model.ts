31516291723fa408754f7dd4e1e5710b7956ef35
Jay-Ubisse <joaquimubisse@gmail.com>
2025-06-10 14:47:18 +0200

    feat: add initial db schemas

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,7 +1,7 @@
 import mongoose, { Document, Schema } from "mongoose";
-import { UserProps } from "../types/user.js";
+import { CategoryProps, ProductProps, UserProps } from "../types/user.js";
 
-/* */
+/* ================== USER =============== */
 
 const userSchema = new Schema<UserProps>({
   name: { type: String, required: true },
@@ -16,3 +16,35 @@ const userSchema = new Schema<UserProps>({
 });
 
 export const User = mongoose.model<UserProps>("User", userSchema);
+
+/* ================== CATEGORY =============== */
+
+const categorySchecma = new Schema<CategoryProps>({
+  name: { type: String, required: true, unique: true },
+  description: { type: String, default: "" },
+});
+
+export const Category = mongoose.model<CategoryProps>(
+  "Category",
+  categorySchecma
+);
+
+/* ================== PRODUCT =============== */
+
+const productSchema = new Schema<ProductProps>({
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: "Category",
+    required: true,
+  },
+  categoryId: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  description: { type: String, default: "" },
+  colors: { type: [String], default: [] },
+  sizes: { type: [String], default: [] },
+  stock: { type: Number, required: true, default: 0 },
+});
+
+export const Product = mongoose.model<ProductProps>("Product", productSchema);
