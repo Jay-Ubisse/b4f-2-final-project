15fd4037e71109262cb3260f082dd5c75e0cc3cc
Moises2304 <ngovenemoises2@gmail.com>
2025-06-10 08:00:51 +0200

    create: category routes

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,3 +1,32 @@
 import { Request, Response } from "express";
-
+import { Category } from "../models/Category.js";
 export function createUser(req: Request, res: Response) {}
+
+
+export const updateCategory = async (req: Request, res: Response): Promise<Response> => {
+
+const { id } = req.params;
+  const { name, description } = req.body;
+
+  try {
+    const category = await Category.findByPk(id);
+    if (!category) {
+      return res.status(404).json({ message: 'Categoria não encontrada' });
+    }
+
+    category.name = name || category.name;
+    category.description = description || category.description;
+    await category.save();
+
+    return res.json({
+      message: 'Categoria atualizada com sucesso',
+      category
+    });
+  } catch (error) {
+    return res.status(500).json({ message: 'Erro ao atualizar categoria', error });
+  }
+};
+
+module.exports = {
+  updateCategory,
+};
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,11 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import mongoose from "mongoose";
+import { router as categoryRoutes } from './routes/category.routes';
 
 const app = express();
 dotenv.config();
 app.use(express.json());
 app.use(cors());
+app.use('/api', categoryRoutes);
 
 const host = process.env.HOST || "http://localhost";
 const port = process.env.PORT || 3000;
