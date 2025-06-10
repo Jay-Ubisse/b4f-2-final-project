84990f2a944139cea60388eca801b6731b34ed83
Gabriel-webmaster <gabrielalfredocuna9@gmail.com>
2025-06-10 16:53:10 +0200

    refactor : update post login

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,7 +1,9 @@
+import  jwt  from 'jsonwebtoken';
 import { Request, Response } from 'express'
 import bcrypt from 'bcrypt'
 import { User } from '../models/user.model.ts'
 import { userProps } from '../types/types.ts'
+
 
 export const login = async (req: Request, res: Response) => {
   const body = req.body
@@ -15,16 +17,27 @@ export const login = async (req: Request, res: Response) => {
       .json({ message: 'Email ou palavra-passe incorreto.' })
   }
 
-  const isEqual = await bcrypt.compare(password, user.password)
-
+  const isEqual = await bcrypt.compare(password, user.password);
   if (!user || !isEqual) {
-    return res
-      .status(401)
-      .json({ message: 'Email ou palavra-passe incorreto.' })
+   return res.status(401).json({ message: "Email ou palavra-passe incorreto." });
   }
 
-  res.status(200).json({ message: 'Seja bem vindo Devolta', user })
-}
+const jwtSecret: string = process.env.JWT_SECRET || '';
+  const token = jwt.sign(
+    {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    },
+   jwtSecret,
+    {
+      expiresIn: "1h",
+    }
+  );
+  res.status(200).json({ message: "Seja bem vindo Devolta", user,token})}
+
+
+
 
 export const register = async (req: Request, res: Response) => {
   try {
