3681a0eaab151183908683a540dcb446f906fb5f
OlgaBernece258 <olgabernecepintomacamo@gmail.com>
2025-06-11 14:30:03 +0200

    feat: delete some repeated lines of code

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -2,21 +2,6 @@ import { Request, Response,NextFunction } from 'express'
 import { User } from '../models/user.model.ts'
 
 
-/*export async function getDetails(req: Request, res: Response) {
-  try {
-    const details = await User.find()
-
-   if (!details) {
-    res.status(404).json({ message: 'Erro, Dados não encontrados' })
-    }
-
-    res.status(200).json({ message: 'Dados encontrados com sucesso', details })
-  } catch (error) {
-    console.log(error)
-    res.status(500).json({ message: 'Erro ao buscar os dados do usuário' })
-  }
-}*/
-
 
 export function getDetails(req: Request, res: Response) {
   try{
