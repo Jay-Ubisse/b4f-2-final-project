02ebc9f12c1e27fbc181cb860c511a6ded702b5d
isaneyde <issitoe941@gmail.com>
2025-06-11 08:42:21 +0200

    feat: add the authentication

--- backend/src/routes/product.route.ts
+++ backend/src/routes/product.route.ts
@@ -5,9 +5,10 @@ import {
   deletedProduct,
   updateProduct,
 } from "../controllers/products.controller.ts";
+import { authentionToken } from "../middleware/auth.middleware.ts";
 export const productRoute=express.Router();
-productRoute.post("/", createProduct);
-productRoute.get("/:id", getProductId);
-productRoute.delete("/:id", deletedProduct);
-productRoute.put("/:id", updateProduct);
+productRoute.post("/", authentionToken, createProduct);
+productRoute.get("/:id",getProductId);
+productRoute.delete("/:id",authentionToken, deletedProduct);
+productRoute.put("/:id", authentionToken, updateProduct);
 
