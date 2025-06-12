c21fc84bad1f12eb1b09b2d7716d21224fdf8a23
neylachavane <neylachavane0@gmail.com>
2025-06-12 14:21:57 +0200

    feat - Created demo components for Button, Dialog, Input, and Label.

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,12 +1,5 @@
 import { Button } from "../components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "../components/ui/dialog"
+
 
 
 export const Home = () => {
@@ -17,19 +10,7 @@ export const Home = () => {
 
      
       
-      <Dialog>
-  <DialogTrigger>Open</DialogTrigger>
-  <DialogContent>
-    <DialogHeader>
-      <DialogTitle>Are you absolutely sure?</DialogTitle>
-      <DialogDescription>
-        This action cannot be undone. This will permanently delete your account
-        and remove your data from our servers.
-      </DialogDescription>
-    </DialogHeader>
-  </DialogContent>
-</Dialog>
-
+      
 
 
 
