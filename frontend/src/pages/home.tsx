e9377b759b37bc263c291eff22e2a5fcc6a1823c
geniamaria <mariageniaabilio@gmail.com>
2025-06-12 13:37:38 +0200

    feat: structure of details

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,6 +1,6 @@
 import { Button } from "../components/ui/button";
 import { login } from "../services/auth";
-import { getProduct } from "../services/products";
+//import { getProduct } from "../services/products";
 
 async function handleLogin() {
   const response = await login({
@@ -17,18 +17,12 @@ async function handleLogin() {
   }
 }
 
-async function handleGetProducts() {
-  const response = await getProduct({ id: "6848315ee273205a2300ef50" });
-}
 export const Home = () => {
   return (
     <>
       <h1 className="bg-amber-500">Home Page</h1>
       <Button onClick={handleLogin} variant={"destructive"}>
         Hello
-      </Button>
-      <Button onClick={handleGetProducts} variant={"destructive"}>
-        Get roducts
       </Button>
     </>
   );
