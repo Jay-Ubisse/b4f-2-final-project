653727bf5da998e2f439e60c043857ccb5d3d68e
neylachavane <neylachavane0@gmail.com>
2025-06-12 17:51:36 +0200

    feat: implement Card component and integrate into Dialog

--- frontend/src/components/ui/myDialog.tsx
+++ frontend/src/components/ui/myDialog.tsx
@@ -1,15 +1,24 @@
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "./dialog";
+import { Card } from "../../components/ui/card";
+import { Button } from "./button";
 
+<Dialog >
+        <div className="h-full max-h-full text-2xl">
+          <DialogTrigger>Cart</DialogTrigger>
 
-<Dialog>
-  <DialogTrigger>Open</DialogTrigger>
-  <DialogContent>
-    <DialogHeader>
-      <DialogTitle>Are you absolutely sure?</DialogTitle>
-      <DialogDescription>
-        This action cannot be undone. This will permanently delete your account
-        and remove your data from our servers.
-      </DialogDescription>
-    </DialogHeader>
-  </DialogContent>
-</Dialog>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Cart</DialogTitle>
+              <DialogDescription>
+                <Card className="w-50  ">
+                  foto
+                </Card>
+                Descricao da peca de roupa
+                <Button variant="link">Remove</Button>
+                <Button>Check out</Button>
+
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </div>
+      </Dialog>
--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -2,19 +2,24 @@ import { Button } from "../components/ui/button";
 
 
 
+
+
 export const Home = () => {
   return (
     <>
       <h1 className="bg-amber-500">Home Page</h1>
       <Button variant={"destructive"}>Hello</Button>
 
-     
-      
       
 
 
 
-   
+
+
+
+
+
+
     </>
   );
 };
