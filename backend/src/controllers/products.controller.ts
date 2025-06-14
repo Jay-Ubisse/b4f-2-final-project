a756af0553b2b305c7cdddbe522de008041e721c
Luis Isaura Vilanculos <luisisauravilanculos@gmail.com>
2025-06-14 11:36:23 +0200

    Update products.controller.ts

--- backend/src/controllers/products.controller.ts
+++ backend/src/controllers/products.controller.ts
@@ -217,3 +217,44 @@ export const getProductsBySearch = async (req: Request, res: Response) => {
      res.status(500).json({ message: "Erro ao buscar produtos." });
   }
 };
+
+export const getProductsByQueryCategory = async (req: Request, res: Response) => {
+  try {
+    const categoryName = req.query.category;
+
+    if (!categoryName || typeof categoryName !== "string") {
+        res.status(400).json({
+        message: "Parâmetro 'categoria' é obrigatório e deve ser string(nome da categoria)."
+      });return
+    }
+    
+    const normalizedCategory = categoryName.trim().toLowerCase();
+
+    const foundCategory = await Category.findOne({
+      name: { $regex: new RegExp(`^${normalizedCategory}$`, "i") }
+    });
+
+    if (!foundCategory) {
+      const allCategories = await Category.find();
+       res.status(404).json({
+        message: `Categoria "${normalizedCategory}" não encontrada.`,
+        availableCategories: allCategories.map(cat => cat.name)
+      });
+    }
+
+    const products = await Products.find({ category: foundCategory?._id });
+
+     res.status(200).json({
+      category: foundCategory?.name,
+      totalProducts: products.length,
+      products
+    });
+
+  } catch (error) {
+    console.error("Erro ao buscar produtos por categoria:", error);
+     res.status(500).json({
+      message: "Erro interno no servidor",
+      error: error instanceof Error ? error.message : "Unknown error"
+    });
+  }
+};
